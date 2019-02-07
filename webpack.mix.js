c0e04a670e0e887bd9cb205747241adcfc6f50f4
Sully Fischer <fischersully@gmail.com>
2019-02-07 19:55:43 +0330

    use mix.version() for cache busting

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,8 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix.ts('resources/ts/app.ts', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+
+if (mix.inProduction()) {
+    mix.version();
+}
